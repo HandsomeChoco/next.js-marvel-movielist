da052657dfe8acb1c57619db8d85fc054a0c1d9a
EvenNewbieImThePro <45553863+EvenNewbieImThePro@users.noreply.github.com>
2020-06-21 23:37:01 +0900

    API 를 통해서 화면 렌더링 구현, 그러나 다른 컴퓨터에서는 Fetch 가 불가능해 NextApp 에 CORS 세팅하러 간다

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,25 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 const app = express();
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+const corsOption = {
+  origin: "*",
+  optionSuccessStatus: 200
+}
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('api/movie/:name', (cors(corsOption), async(req, res, next) => { }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { faBars, faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment';
+import index from './index.module.css'
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: {
-      }
+      movies: []
     }
+    this._callApi = this._callApi.bind(this);
+    this._getMovies = this._getMovies.bind(this);
+    this._renderMovies = this._renderMovies.bind(this);
   }
+
   componentDidMount() {
-    console.log('mount')
     this._getMovies();
   }
 
@@ -29,24 +32,52 @@ class Home extends React.Component {
     this.setState({ movies: movies});
   }
   
-  _renderMovie = () => {
-    return(
-      <div>
-        {this.state.movies.movieListResult.movieList.map((data, index) => {
-          <div key={index}>
-            {data}
-          </div>
-        })}
-      </div>
-    );
+  _renderMovies = () => {
+    const list = this.state.movies.map((data, index) => {
+      return(
+        <Movie
+          key={index}
+          movieTitle={data.title}
+          year={data.year}
+          mainCasting={data.mainCasting}
+          likes={data.like}
+          dislikes={data.dislike}
+          score={data.score}
+          runningTime={data.runningTime}
+          degree={data.degree}
+        />
+      );
+    })
+    return list;
   }
+
   render() {
     return (
       <Layout title={'index'}>
-        {this.state.movies.movieListResult ? this._renderMovie() : 'nothing to render'}
+        <div className={index.movieList}>
+          {this.state.movies.length>0 ? this._renderMovies() : 'nothing to render'}
+        </div>
       </Layout>
     );
   }
+}
+
+function Movie({ index, movieTitle, year, mainCasting, likes, dislikes, score, runningTime, degree}) {
+  return(
+    <div key={index}>
+      <img src={`/imgs/poster/${movieTitle}.png`} alt={movieTitle} title={movieTitle}/>
+      <div>
+        <h1>{movieTitle} ({year}) </h1>
+      </div>
+      <div>
+        <p>주연: {mainCasting}</p>
+      </div>
+      <div>
+        <span>좋아요: {likes}</span> <span>싫어요: {dislikes}</span> <span>평점: {score}</span>
+        <span>상영시간: {runningTime}분</span> <span>등급: {degree}세 관람가</span>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
