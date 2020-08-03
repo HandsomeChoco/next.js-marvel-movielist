efd645ad8875a2c6ab27154af9e6c62c8d713de4
HandsomeChoco <kkb931115@gmail.com>
2020-08-03 18:26:04 +0900

    영화 동적 라우팅 페이지 갈아 엎음

--- client/pages/movie/[title].js
+++ client/pages/movie/[title].js
@@ -7,23 +7,24 @@ import moment from 'moment';
 import Axios from 'axios';
 
 import FontAwesome, { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons'
-import React, {useState} from 'react'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
+import React, {useState, useEffect} from 'react';
 
 export async function getServerSideProps(context) {
   let data, review;
   try {
     data = await fetch(`http://10.10.12.3:4000/api/movie/${encodeURIComponent(context.params.title)}`)
                 .then(response => response.json());
+
     review = await fetch(`http://10.10.12.3:4000/api/review`)
       .then(response => response.json());
-    
   } catch(err) {
     return err;
   }
+
   return {
     props: { 
-      data,
+      data: data[0],
     }
   }
 }
@@ -31,188 +32,29 @@ export async function getServerSideProps(context) {
 const Movie = ( data, review ) => {
   const router = useRouter();
   const { title } = router.query;
-  const movieData = data.data[0];
+  const movieData = data.data;
   const reviewData = review[0];
+
   return (
     <Layout 
       title={`상세 정보: ${title}`}
       childComponent={
-        <ChildOfLayout 
-          prop={movieData}
-          //review={review}
+        <Container
+          _movieData={movieData}
         />
       }
     />
   );
 }
 
-
-function ChildOfLayout({ prop, review }) {
-  return(
-    <div>
-      <div className={titleCSS.movieMetaDataWrapper}>
-        <MoviePoster _movieTitle={prop.title} _path={prop.imageFileName} _degreeIcon={prop.degree}/>
-        <MovieInfo 
-          _id={prop._id}
-          _title={prop.title} 
-          _enTitle={prop.enTitle}
-          _year={prop.year}
-          _director={prop.director}
-          _likesCount={prop.likes.length}
-          _dislikesCount={prop.dislikes.length}
-          _score={prop.score}
-          _runningTime={prop.runningTime}
-          _casting={prop.casting}
-        />
-      </div>
-      <div className={titleCSS.subTitle}/>
-      <Review/>
-    </div>
-  );
-}
-
-function MoviePoster({ _path, _movieTitle, _degreeIcon }) {
-  function getColor(age) {
-    let color;
-    if(age==='all') {
-      color='#3fa449';
-    } else if (age==12) {
-      color='#22a2dc';
-    } else if (age==15) {
-      color='#efa126';
-    } else if (age==18) {
-      color='#cb2126';
-    }
-    return color;
-  }
+const Container = (_movieData) => {
   return (
-    <div className={titleCSS.moviePoster}>
-      <div>
-        <span className={titleCSS.degree} style={{backgroundColor:getColor(_degreeIcon)}}>
-          <b>{_degreeIcon}</b>
-        </span>
-        <img src={`/imgs/poster/${_path}`} alt={_movieTitle} title={_movieTitle+' 포스터'} />
-      </div>
+    <div 
+      className={titleCSS.container} 
+      style={{backgroundImage:`url('/imgs/poster/${_movieData._movieData.imageFileName}')`}}
+    >
+      <h1>{_movieData._movieData.title}</h1>
     </div>
-  );
-}
-
-function MovieInfo({ _id, _title, _enTitle, _year, _degreeIcon, _director, _likesCount, _dislikesCount, _score, _runningTime, _casting }) {
-  return(
-    <div className={titleCSS.movieData}>
-      {_id ? <input type="hidden" name="movieId" value={_id}/> : ''}
-      <MovieTitle
-        __degreeIcon={_degreeIcon}
-        __title={_title}
-        __enTitle={_enTitle}
-        __year={_year}
-      />
-      <MovieMetaData
-        __likes={_likesCount}
-        __dislikes={_dislikesCount}
-        __director={_director}
-        __score={_score}
-        __runningTime={_runningTime}
-        __casting={_casting}
-      />
-    </div>
-  );
-}
-
-function MovieTitle({__title, __enTitle, __year }) {
-  const year = moment(__year).format('YYYY');
-  return(
-    <div>
-      <h1> 
-        {__title}
-      </h1>
-      <div className={titleCSS.subTitle}>
-        {__enTitle}, {year}
-      </div>
-    </div>
-  );
-}
-
-function MovieMetaData({ __likes, __dislikes, __director, __score, __runningTime, __casting }) {
-  function isListWithPic(prop) {
-    const list = prop.map((v, index) => {
-      return(
-        <Link href={`/actor/${v}`} key={index}>
-          <a className={titleCSS.casting}>
-            <li>
-              <div>
-                <img 
-                  alt={v} 
-                  src={`/imgs/actor/${v}.jfif`} 
-                  className={titleCSS.profilePic}
-                />
-                <div className={titleCSS.actorName} title={v}>
-                  {v}
-                </div>
-              </div>
-              
-            </li>
-          </a>
-        </Link>
-      );
-    })
-    return list;
-  }
-  
-  return(
-    <div className={titleCSS.movieMetaData}>
-      <div style={{textAlign:'left'}}> 감독: <ul className={titleCSS.directorUL}>{isListWithPic(__director)}</ul></div>
-      <div className={titleCSS.tasty}>
-        <div><FontAwesomeIcon icon={faThumbsUp} className={titleCSS.likes}/> {__likes} </div>
-        <div><FontAwesomeIcon icon={faThumbsDown} className={titleCSS.dislikes}/> {__dislikes}</div>
-      </div>
-      
-      <div className={titleCSS.movieInfo}>
-        <div className={titleCSS.score}>평점: {__score}</div>
-        <div className={titleCSS.runningTime}>상영시간: {__runningTime}분</div>
-        <div style={{textAlign:'left'}}> 출연진: <ul className={titleCSS.castingUL}>{isListWithPic(__casting, true)}</ul></div>
-      </div>
-    </div>
-  );
-}
-
-function Review({ }) {
-  return(
-    <div>
-      <ul>
-        <li>영화리뷰</li>
-        <li>영화리뷰</li>
-        <li>영화리뷰</li>
-        <li>영화리뷰</li>
-        <li>영화리뷰</li>
-      </ul>
-      <WriteReview/>
-    </div>
-  );
-}
-
-function WriteReview() {
-  const [review, setReview ] = useState(''); // hook
-
-  return(
-    <form 
-      action="/api/comment" 
-      method="POST"
-      onSubmit={(e) => {e.preventDefault();}}
-      className={titleCSS.reviewForm}
-    > 
-      <div className={titleCSS.writeReviewWrapper}>
-        <textarea 
-          placeholder={'리뷰를 작성해볼까요?'}
-          onChange={(e)=> {setReview(e.target.value);}}
-        />
-        <button type="submit" className={titleCSS.submitBtn}>작성</button>
-      </div>
-      
-      <div>
-        {review.length}/300
-      </div>
-    </form>
   );
 }
 
