e9cf08caf8cb5cc4a64d029bd0b32c52067a142d
KangbeomKim <kkb931115@gmail.com>
2020-08-06 04:02:52 +0900

    동적 라우팅 페이지 다시 바꾸는 중...

--- client/pages/movie/[title].js
+++ client/pages/movie/[title].js
@@ -8,54 +8,85 @@ import Axios from 'axios';
 
 import FontAwesome, { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 export async function getServerSideProps(context) {
-  let data, review;
-  try {
-    data = await fetch(`http://10.10.12.3:4000/api/movie/${encodeURIComponent(context.params.title)}`)
-                .then(response => response.json());
+	let data, review;
+	try {
+		data = await fetch(
+			`http://10.10.12.3:4000/api/movie/${encodeURIComponent(
+				context.params.title
+			)}`
+		).then((response) => response.json());
 
-    review = await fetch(`http://10.10.12.3:4000/api/review`)
-      .then(response => response.json());
-  } catch(err) {
-    return err;
-  }
+		review = await fetch(`http://10.10.12.3:4000/api/review`).then((response) =>
+			response.json()
+		);
+	} catch (err) {
+		return err;
+	}
 
-  return {
-    props: { 
-      data: data[0],
-    }
-  }
+	return {
+		props: {
+			data: data[0],
+			review,
+		},
+	};
 }
 
-const Movie = ( data, review ) => {
-  const router = useRouter();
-  const { title } = router.query;
-  const movieData = data.data;
-  const reviewData = review[0];
+const Movie = ({ data, review }) => {
+	const router = useRouter();
+	const { title } = router.query;
 
-  return (
-    <Layout 
-      title={`상세 정보: ${title}`}
-      childComponent={
-        <Container
-          _movieData={movieData}
-        />
-      }
-    />
-  );
-}
+	return (
+		<Layout
+			title={`상세 정보: ${title}`}
+			childComponent={<Container data={data} />}
+		/>
+	);
+};
 
-const Container = (_movieData) => {
-  return (
-    <div 
-      className={titleCSS.container} 
-      style={{backgroundImage:`url('/imgs/poster/${_movieData._movieData.imageFileName}')`}}
-    >
-      <h1>{_movieData._movieData.title}</h1>
-    </div>
-  );
-}
+const Container = ({ data }) => {
+	const imgFileName = data.imageFileName;
+	const title = data.title;
+	const enTitle = data.enTitle;
+
+	return (
+		<main className={titleCSS.container}>
+			<img
+				className={titleCSS.poster}
+				src={`/imgs/poster/${imgFileName}`}
+				alt={`/imgs/poster/${imgFileName}`}
+				srcSet=''
+			/>
+			<article className={titleCSS.wrapper}>
+				<Titles title={title} enTitle={enTitle} />
+			</article>
+		</main>
+	);
+};
+
+const Titles = ({ title, enTitle }) => {
+	function showTitle(arg) {
+		return arg ? arg : '제목 정보를 가져오지 못했습니다.';
+	}
+
+	return (
+		<header>
+			<h1 className={`${titleCSS.title} ${titleCSS.kr}`}>{showTitle(title)}</h1>
+			<h6 className={`${titleCSS.title} ${titleCSS.en}`}>
+				{showTitle(enTitle)}
+			</h6>
+		</header>
+	);
+};
+
+const MetaData = ({}) => {
+	return (
+		<div>
+			<h1>asddsad</h1>
+		</div>
+	);
+};
 
 export default Movie;
