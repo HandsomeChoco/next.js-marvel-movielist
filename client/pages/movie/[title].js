998e77a6e1fa4f1d868bfb4487884fa3b2f7326a
KangbeomKim <kkb931115@gmail.com>
2020-08-07 22:46:16 +0900

    마지막으로 갈아 엎기전과  비슷한 구조로 리팩토링

--- client/pages/movie/[title].js
+++ client/pages/movie/[title].js
@@ -1,14 +1,15 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import titleCSS from './title.module.css';
-
+import components from '../../components/lib/component';
+import lib from '../../components/lib/lib';
 import moment from 'moment';
 import Axios from 'axios';
 
 import FontAwesome, { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context) {
 	let data, review;
@@ -47,44 +48,106 @@ const Movie = ({ data, review }) => {
 };
 
 const Container = ({ data }) => {
-	const imgFileName = data.imageFileName;
-	const title = data.title;
-	const enTitle = data.enTitle;
-
 	return (
-		<main className={titleCSS.container}>
-			<img
-				className={titleCSS.poster}
-				src={`/imgs/poster/${imgFileName}`}
-				alt={`/imgs/poster/${imgFileName}`}
-				srcSet=''
-			/>
-			<article className={titleCSS.wrapper}>
-				<Titles title={title} enTitle={enTitle} />
-			</article>
-		</main>
+		<>
+			<main className={titleCSS.container}>
+				<Article data={data} />
+				<content>
+					<Titles data={data} />
+					<Casting data={data} />
+					<Review data={data} />
+				</content>
+			</main>
+		</>
 	);
 };
 
-const Titles = ({ title, enTitle }) => {
-	function showTitle(arg) {
-		return arg ? arg : '제목 정보를 가져오지 못했습니다.';
-	}
+const Article = ({ data }) => {
+	const imgFileName = data.imageFileName;
+	const title = data.title;
+	return (
+		<article className={titleCSS.wrapper}>
+			<img
+				src={`/imgs/poster/${imgFileName}`}
+				alt={title}
+				srcSet=''
+				className={titleCSS.poster}
+			/>
+			<Tasty data={data} />
+		</article>
+	);
+};
+
+const Tasty = ({ data }) => {
+	const likes = data.likes.length;
+	const dislikes = data.dislikes.length;
 
 	return (
-		<header>
-			<h1 className={`${titleCSS.title} ${titleCSS.kr}`}>{showTitle(title)}</h1>
+		<div>
+			<div className={titleCSS.tasty}>
+				{components.CreateIcon(
+					faThumbsUp,
+					titleCSS.like,
+					lib.kFormatter(likes)
+				)}
+				{components.CreateIcon(
+					faThumbsDown,
+					titleCSS.dislike,
+					lib.kFormatter(dislikes)
+				)}
+			</div>
+		</div>
+	);
+};
+
+const Titles = ({ data }) => {
+	const year = moment(data.year).format('YYYY');
+	const title = data.title;
+	const enTitle = data.enTitle;
+	const errText = '제목 정보를 가져오지 못했습니다.';
+	return (
+		<header className={titleCSS.titleWrap}>
+			<h3 className={`${titleCSS.title} ${titleCSS.kr}`}>
+				{lib.showText(title)}
+			</h3>
 			<h6 className={`${titleCSS.title} ${titleCSS.en}`}>
-				{showTitle(enTitle)}
+				{lib.showText(enTitle, errText)}, ({year})
 			</h6>
 		</header>
 	);
 };
 
-const MetaData = ({}) => {
+const Casting = ({ data }) => {
+	const director = data.director;
+	const mainCasting = data.mainCasting;
+	const allCasting = data.casting;
+
+	function casting(arr) {
+		const list = arr.map((v, i) => {
+			return (
+				<li key={i} className={titleCSS.item}>
+					<img src={`/imgs/actor/${v}.jfif`} alt={i} srcSet='' />
+					<span>{v}</span>
+				</li>
+			);
+		});
+		return list;
+	}
+
 	return (
 		<div>
-			<h1>asddsad</h1>
+			<h3>감독</h3>
+			<ul className={titleCSS.list}>{casting(director)}</ul>
+			<h3>주연</h3>
+			<ul className={titleCSS.list}>{casting(mainCasting)}</ul>
+		</div>
+	);
+};
+
+const Review = ({ data }) => {
+	return (
+		<div>
+			<span>asdf</span>
 		</div>
 	);
 };
