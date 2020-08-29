ae76dd662f6091b550ef932366af56495d582c2d
HandsomeChoco <kkb931115@gmail.com>
2020-08-30 03:47:31 +0900

    레이아웃의 border 속성 제거 및 sidebar 조금 작업

--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -8,7 +8,13 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 function SideBar() {
   return(
     <div id={sidebar.container}>
-
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
