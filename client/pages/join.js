443aa63b3ece02a85590e096796f1f62822e3dcf
HandsomeChoco <kkb931115@gmail.com>
2020-09-06 03:21:07 +0900

    join 페이지에서 회원 가입 폼을 통해 백엔드 서버로 데이터를 보낼 수 있도록 이벤트 핸들러 작성
    약관에 동의한 후 확인 버튼을 눌러야 회원가입 폼 컴포넌트가 보이도록 로직수정 및 리팩토링

--- client/pages/join.js
+++ client/pages/join.js
@@ -8,92 +8,121 @@ import login from './login.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 
+// 클래스로 만들어보고 싶어서 클래스 써봄.
 class Join extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			readTerms: false,
-			isPossibleToAgree: false,
+			isAbleToAgree: false,
+			renderComponent: 'Terms',
 			terms: '',
-			password: '',
-			passwordCheck: '',
+			inputs : {
+				id: '',
+				password: '',
+				passwordCheck: '',
+			}
 		};
-		// this._handleTerms = this._handleTerms.bind(this);
+		this._handleAgree = this._handleAgree.bind(this);
 		this._loadTerms = this._loadTerms.bind(this);
-		this._handlePassWord = this._handlePassWord.bind(this);
-		this._handlePassWordCheck = this._handlePassWordCheck.bind(this);
+		this._handleInput = this._handleInput.bind(this);
+		this._handleComponent = this._handleComponent.bind(this);
+		this._handleSubmit = this._handleSubmit.bind(this);
+
 	}
 
 	componentDidMount() {
 		this._loadTerms();
 	}
-
-	_handleTerms = () => {
-		this.setState({
-			readTerms: !this.state.readTerms,
-		});
-	};
-	_handleAgree(e) {
-		this.setState({
-			isPossibleToAgree: !this.state.isPossibleToAgree,
-		});
+	componentDidUpdate(prevProps, prevState) {
+		console.log(prevProps, prevState)
 	}
 	_callTermAPI() {
 		return fetch(
 			'http://10.10.12.3:4000/api/policy/join_term'
 		).then((response) => response.json());
-		console.log(response);
 	}
-	_loadTerms = async () => {
+	_loadTerms = async() => {
 		const terms = await this._callTermAPI();
 		this.setState({
 			terms: terms,
 		});
 	};
-	_handlePassWord(e) {
+	_handleAgree() {
 		this.setState({
-			password: e.target.value,
+			isAbleToAgree: !this.state.isAbleToAgree,
 		});
 	}
-	_handlePassWordCheck(e) {
+	_handleComponent() {
+		if(this.state.renderComponent==='Terms') {
+			return this.setState({
+				renderComponent: 'JoinForm'
+			})
+		} else return this.setState({
+			renderComponent: 'Terms'
+		})
+	}
+	_handleInput(e) {
+		const { name, value } = e.target;
 		this.setState({
-			passwordCheck: e.target.value,
+			...this.state,
+			inputs: {
+				...this.state.inputs,
+				[name]: value
+			}
 		});
 	}
+	_handleSubmit = async(e) => {
+		e.preventDefault();
+		const url = `http://10.10.12.3:4000/api/user`;
+		await Axios.post(url,{
+			id: this.state.inputs.id,
+			password: this.state.inputs.password,
+			passwordCheck: this.state.inputs.passwordCheck
+		});
+	}
+
 	render() {
+		const { terms, isAbleToAgree, renderComponent } = this.state;
+		const { id, password, passwordCheck } = this.state.inputs;
+		const title = renderComponent === 'JoinForm' ? '회원 정보 입력' : '이용 약관 확인';
 		return (
-			<Layout title={'Join to our universe'}>
-				{this.state.readTerms === false ? (
-					<Terms
-						handleState={this._handleTerms}
-						getTermsProp={this.state.terms}
-					/>
-				) : (
-					<JoinForm
-						_cancel={this._handleTerms}
-						handlePassWord={this._handlePassWord}
-						handleValidPassWord={this._handlePassWordCheck}
-						passwordProp={this.state.password}
-						passwordCheckProp={this.state.passwordCheck}
-					/>
-				)}
+			<Layout title={title}>
+				{renderComponent==='JoinForm' ?
+					(<JoinForm
+						cancel={this._handleTerms}
+						onSubmit={this._handleSubmit}
+						onChange={this._handleInput}
+						id={id}
+						password={password}
+						passwordChk={passwordCheck}
+						handleSubmit={this._handleSubmit}
+					/>) 
+				:
+					(<Terms
+						handleComponent={this._handleComponent}
+						getTerms={this.state.terms}
+						handleAgree={this._handleAgree}
+						agreeProp = {this.state.isAbleToAgree}
+					/>)	
+				}
 			</Layout>
 		);
 	}
 }
 
 function JoinForm({
-	handleSendReq,
-	handlePassWord,
-	handleValidPassWord,
+	onSubmit,
+	onChange,
+	id,
 	cancel,
-	passwordProp,
-	passwordCheckProp,
+	password,
+	passwordCheck,
 }) {
 	function PwCheck() {
 		let pw;
-		if (passwordCheckProp && passwordProp) {
-			if (passwordProp === passwordCheckProp) {
+		console.log(password, passwordCheck)
+		if (passwordCheck && password) {
+			if (password === passwordCheck) {
 				pw = '비밀번호가 일치합니다.';
 			} else pw = '비밀번호가 일치하지 않습니다.';
 		}
@@ -103,37 +132,45 @@ function JoinForm({
 	return (
 		<div>
 			<div className={login.loginInputBox}>
-				<form method='post'>
+				<form 
+					method='post'
+					onSubmit={onSubmit}
+				>
 					<div className={login.loginEachInput}>
 						<input
 							className='inputInfo'
+							name="id"
 							maxLength='20'
 							placeholder='계정이름'
+							onChange={onChange}
+							value={id}
 						/>
 					</div>
 
 					<div className={login.loginEachInput}>
 						<input
 							className='inputInfo'
+							name="password"
 							type='password'
 							maxLength='20'
 							placeholder='비밀번호'
-							onChange={handlePassWord}
+							onChange={onChange}
 						/>
 					</div>
 					<div className={login.loginEachInput}>
 						<input
 							className='inputInfo'
+							name="passwordCheck"
 							type='password'
 							maxLength='20'
 							placeholder='비밀번호 확인'
-							onChange={handleValidPassWord}
+							onChange={onChange}
 						/>
 						{PwCheck()}
 					</div>
 
 					<div className={login.loginButton}>
-						<button onClick={handleSendReq}>회원가입</button>
+						<button type="submit">회원가입</button>
 						<button className={login.returnButton} onClick={cancel}>
 							약관으로 돌아가기
 						</button>
@@ -144,23 +181,23 @@ function JoinForm({
 	);
 }
 
-function Terms({ handleState, getTermsProp }) {
+function Terms({ handleComponent, getTerms, handleAgree, agreeProp }) {
+	const disabled = <button className={join.joinCancel} onClick={() => alert('이용 약관에 동의해 주세요')}>확인</button>;
+	const able = <button className={join.joinSubmit} onClick={handleComponent}>확인</button>
 	return (
 		<div>
 			<div className={join.readTerms}>
-				{getTermsProp ? getTermsProp : 'loading...'}
+				{getTerms ? getTerms : '이용 약관을 불러오는 중입니다.'}
 			</div>
-			<div>
-				<label>
-					<input type={'checkbox'} title={'약관동의'} />
+			<div style={{ padding: '10px 0px' }}>
+				<label> {/*라벨 태그에 이벤트 걸면 두 번 실행되니 조심 */}
+					<input type={'checkbox'} title={'약관동의'} onClick={handleAgree} /> 
 					위의 이용약관에 동의합니다.
 				</label>
 			</div>
 			<div className={join.agreement}>
 				<div>
-					<button className={join.joinSubmit} onClick={handleState}>
-						확인
-					</button>
+					{ agreeProp ? able : disabled }
 					<button className={join.joinCancel}>취소</button>
 				</div>
 			</div>
